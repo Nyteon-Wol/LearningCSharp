6c091c9846e4a5ba840c6ae194db1a30360e6099
Matheus <MatheusEdDominguesGanancio@Gmail.com>
2024-09-30 21:54:36 -0300

    Add files via upload

---
+++ CursoCSharp/ClassesEMetodos/MetodosEstaticos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    public class CalculadoraEstatica
+    {
+        /*Metodo de classe ou Metodo estático*/
+        public static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+        /*Metodo de instância */
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    class MetodosEstaticos
+    {
+        public static void Executar()
+        {
+            var resultado = CalculadoraEstatica.Multiplicar(2, 2);
+            Console.WriteLine("Resultado: {0}", resultado);
+
+            CalculadoraEstatica calc = new CalculadoraEstatica();
+            Console.WriteLine(calc.Somar(2, 2));
+            Console.WriteLine(CalculadoraEstatica.Multiplicar(2, 2));
+        }
+    }
+}
