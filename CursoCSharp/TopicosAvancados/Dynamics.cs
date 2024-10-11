dd165738525724702748ec326d2cdb21899d1ca8
Matheus <MatheusEdDominguesGanancio@Gmail.com>
2024-10-11 09:55:01 -0300

    Add files via upload

---
+++ CursoCSharp/TopicosAvancados/Dynamics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.TopicosAvancados
+{
+    class Dynamics
+    {
+        public static void Executar()
+        {
+            dynamic meuObjeto = "teste";
+            meuObjeto = 3;
+
+            meuObjeto++;
+            Console.WriteLine(meuObjeto);
+
+            dynamic aluno = new System.Dynamic.ExpandoObject();
+            aluno.nome = "Maria Julia";
+            aluno.nota = 5.7;
+            aluno.idade = 15;
+
+            Console.WriteLine($"{aluno.nome} {aluno.nota} {aluno.idade}");
+        }
+    }
+}
